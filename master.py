96fa0aecb8e54b8c093124ca3f38bef19aeec64c
NoelSabia <noel.julian.sabia@gmail.com>
2025-02-21 22:23:14 +0100

    updated readme and implemented the browser

--- master.py
+++ master.py
@@ -12,6 +12,7 @@ init()
 class MasterOrganizer:
 	def __init__(self) -> None:
 		self._directory = []
+		self._browser = []
 		self._websites = []
 
 	# Load the program arguments into the master organizer object
@@ -26,6 +27,7 @@ class MasterOrganizer:
 			blocks = f.read().strip().split("\n\n")
 			for block in blocks:
 				block_dir = None
+				block_browser = None
 				block_websites = []
 
 				# Parse each line with the regex
@@ -38,6 +40,10 @@ class MasterOrganizer:
 							if block_dir is not None:
 								raise ValueError("Error: Block contains more than one 'dir'.")
 							block_dir = value
+						elif key == "browser":
+							if block_browser is not None:
+								raise ValueError("Error: Block contains more than one 'browser'.")
+							block_browser = value
 						elif key == "website":
 							block_websites.append(value)
 
@@ -51,6 +57,7 @@ class MasterOrganizer:
 
 				# Append to internal lists
 				self._directory.append(block_dir)
+				self._browser.append(block_browser)
 				self._websites.append(block_websites)
 
 	# Signal handler function to catch SIGINT and SIGTERM
@@ -61,17 +68,17 @@ class MasterOrganizer:
 		:param frame: current stack frame
 		:return:
 		"""
-		print(Fore.GREEN + "\nSIGINT or SIGTERM received. Cleaning up resources." + Style.RESET_ALL)
+		print(Fore.GREEN + "\n\nSIGINT or SIGTERM received. Cleaning up resources." + Style.RESET_ALL)
 
 		# Clean the directories from self._directory
 		for directory in self._directory:
 			try:
 				shutil.rmtree(Path(directory).expanduser())
-				print(Fore.GREEN + f"Deleted directory: {directory}" + Style.RESET_ALL)
+				print(Fore.GREEN + f"\nDeleted directory: {directory}" + Style.RESET_ALL)
 			except FileNotFoundError:
-				print(Fore.YELLOW + f"Directory not found: {directory}, skipping." + Style.RESET_ALL)
+				print(Fore.YELLOW + f"\nDirectory not found: {directory}, skipping." + Style.RESET_ALL)
 			except PermissionError:
-				print(Fore.RED + f"Insufficient permissions to delete: {directory}" + Style.RESET_ALL)
+				print(Fore.RED + f"\nInsufficient permissions to delete: {directory}" + Style.RESET_ALL)
 
 		# Remove __pycache__ directories
 		for folder in ["voice_gathering", "info_gathering", "video_gathering"]:
@@ -83,20 +90,20 @@ class MasterOrganizer:
 def main() -> None:
 
 	# Create an instance of the MasterOrganizer class
-    master_org = MasterOrganizer()
-    master_org.load_args_into_master_org()
+	master_org = MasterOrganizer()
+	master_org.load_args_into_master_org()
 
-    # Register signal handlers using the instance method
-    signal.signal(signal.SIGINT, master_org.signal_handler)
-    signal.signal(signal.SIGTERM, master_org.signal_handler)
+	# Register signal handlers using the instance method
+	signal.signal(signal.SIGINT, master_org.signal_handler)
+	signal.signal(signal.SIGTERM, master_org.signal_handler)
 
 	# Run main.py with the paths and websites
-    for i, path in enumerate(master_org._directory):
-        print(f"\nRunning main.py with path={path} and websites={master_org._websites[i]}")
-        websites = master_org._websites[i]
-        command = ["python3", "main.py", path] + websites
-        subprocess.run(command)  # wait for main.py to finish before proceeding
-        print(f"\nmain.py with path={path} and websites={websites} finished.")
+	for i, path in enumerate(master_org._directory):
+		print(Fore.GREEN + f"\nRunning main.py with path={path}, browser={master_org._browser[i]} and website(s)={master_org._websites[i]}" + Style.RESET_ALL)
+		websites = master_org._websites[i]
+		command = ["python3", "main.py", path, master_org._browser[i]] + websites
+		subprocess.run(command)  # wait for main.py to finish before proceeding
+		print(f"\nmain.py with path={path} and websites={websites} finished.")
 
 if __name__ == "__main__":
-    main()
+	main()
